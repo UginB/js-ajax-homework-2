8a7b31dacf3ea15c778704e571fbfe2748f46b1e
Evgeniy Bratenkin <ugin-bro@ya.ru>
2023-01-15 21:23:47 +0300

    add deep copy

--- js/index.js
+++ js/index.js
@@ -1,17 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const cube = document.querySelector(".elemRespown__cube");
+  const respown = document.querySelector(".elemRespown");
+  const cube = document.querySelector(".elemRespown__cube_active");
   const fields = document.querySelector(".fields");
 
-  const mouseMoveListener = (e) => {
-    cube.style.left = e.pageX - 50 + "px";
-    cube.style.top = e.pageY - 50 + "px";
+  let mouseMoveListener;
+  let deepCopyCube;
+
+  const createCloneCube = () => {
+    deepCopyCube = cube.cloneNode(true);
+    deepCopyCube.classList.remove("elemRespown__cube_active");
+    deepCopyCube.style.position = "absolute";
+    respown.append(deepCopyCube);
+    mouseMoveListener = (e) => {
+      deepCopyCube.style.left = e.pageX + "px";
+      deepCopyCube.style.top = e.pageY + "px";
+    };
   };
 
   cube.addEventListener("mousedown", (e) => {
-    cube.style.position = "absolute";
+    createCloneCube();
     document.addEventListener("mousemove", mouseMoveListener);
   });
-  cube.addEventListener("mouseup", (e) => {
+  deepCopyCube.addEventListener("mouseup", (e) => {
     document.removeEventListener("mousemove", mouseMoveListener);
   });
 });
