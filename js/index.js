5f0db7e16d9793898244c6269d3c61df63c34fc6
Evgeniy Bratenkin <ugin-bro@ya.ru>
2023-01-16 19:31:42 +0300

    add rules

--- js/index.js
+++ js/index.js
@@ -1,28 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
   const respown = document.querySelector(".elemRespown");
   const cube = document.querySelector(".elemRespown__cube_active");
-  const fields = document.querySelector(".fields");
+  const fieldFree = document.querySelector(".fields__field_free");
+  const fieldGrid = document.querySelector(".fields__field_grid");
 
   let mouseMoveListener;
   let deepCopyCube;
 
   const createCloneCube = () => {
     deepCopyCube = cube.cloneNode(true);
-    deepCopyCube.classList.remove("elemRespown__cube_active");
-    deepCopyCube.style.position = "absolute";
+    deepCopyCube.classList.add("elemRespown__cube_abs");
     respown.append(deepCopyCube);
     mouseMoveListener = (e) => {
-      deepCopyCube.style.left = e.pageX + "px";
-      deepCopyCube.style.top = e.pageY + "px";
+      deepCopyCube.style.left = e.pageX + 1 + "px";
+      deepCopyCube.style.top = e.pageY + 1 + "px";
     };
+  };
+
+  const mouseUpHandler = (e) => {
+    console.log(e.target);
+    if (e.target != fieldFree) {
+      deepCopyCube.remove();
+    }
+    if (e.target === fieldGrid) {
+      fieldGrid.append(cube.cloneNode(true));
+    }
+    document.removeEventListener("mousemove", mouseMoveListener);
+    document.body.removeEventListener("mouseup", mouseUpHandler);
   };
 
   cube.addEventListener("mousedown", (e) => {
     createCloneCube();
     document.addEventListener("mousemove", mouseMoveListener);
-  });
-  deepCopyCube.addEventListener("mouseup", (e) => {
-    document.removeEventListener("mousemove", mouseMoveListener);
+    document.body.addEventListener("mouseup", mouseUpHandler);
   });
 });
-//   console.log("mouse location:", e.clientX, e.clientY);
